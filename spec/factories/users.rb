c1760b5216a9ac6354e0187f10d185290a0ba189
tomo111111 <ttt0206sss@gmail.com>
2020-08-18 14:08:33 +0900

    FactoryBotの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {Faker::Games::Pokemon.name}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {"田中"}
+    first_name            {"太郎"}
+    last_name_reading     {"タナカ"}
+    first_name_redading   {"タロウ"}
+    birthday              {"2000-06-06"}
   end
 end
