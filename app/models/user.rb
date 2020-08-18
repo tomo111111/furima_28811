a209389226ff8206eb897200164612ca152065ff
tomo111111 <ttt0206sss@gmail.com>
2020-08-18 17:24:50 +0900

    Userモデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :email,uniqueness: true
+    validates :nickname
     validates :password,format:{with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/}
     validates :last_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :birthday
   end       
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Games::Pokemon.name}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {"abc123"}
     password_confirmation {password}
     last_name             {"田中"}
     first_name            {"太郎"}
     last_name_reading     {"タナカ"}
-    first_name_redading   {"タロウ"}
+    first_name_reading   {"タロウ"}
     birthday              {"2000-06-06"}
   end
 end
