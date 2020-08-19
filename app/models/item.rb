87c5bf7cf35e5d7d42845b3f9b8f1308e2ec18de
tomo111111 <ttt0206sss@gmail.com>
2020-08-19 17:49:33 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,15 +8,20 @@ class Item < ApplicationRecord
   belongs_to_active_hash :ship_from
   belongs_to_active_hash :ship_date
 
-  validates :name, presence: true
-  # with_options presence: true do
-  #   validates :name
-  #   validates :text
-  #   validates :category
-  #   validates :condition
-  #   validates :postage
-  #   validates :ship_from
-  #   validates :ship_date
-  #  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
-  # end
+    
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :text
+    validates :category_id,numericality: { other_than: 1 }
+    validates :condition_id,numericality: { other_than: 1 }
+    validates :postage_id,numericality: { other_than: 1 }
+    validates :ship_from_id,numericality: { other_than: 1 }
+    validates :ship_date_id,numericality: { other_than: 1 }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+
+    # def was_attached?
+    #   self.image.attached?
+    # end
+  end
 end
