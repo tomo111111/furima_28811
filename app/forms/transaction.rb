1556b29271e7b5db27b6d5a1b1aaca3acbc441ad
tomo111111 <ttt0206sss@gmail.com>
2020-09-02 16:29:40 +0900

    日本語化処理

--- app/forms/transaction.rb
+++ app/forms/transaction.rb
@@ -4,8 +4,8 @@ class Transaction
 
   with_options presence: true do
     validates :token
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'は無効です。***-****の形式で入力してください' }
+    validates :prefecture_id, numericality: { other_than: 1,message: 'を選択してください' }
     validates :city
     validates :house_number
     validates :phone_number, format: { with: /\A\d{11}\z/ }
